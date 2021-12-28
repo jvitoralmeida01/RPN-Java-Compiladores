aceaa73893f93668f3930a8809908ec09db8dfce
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2021-12-28 00:19:15 -0300

    adicionando task 03

---
+++ Task03/src/interpreter/InterpreterError.java
@@ -0,0 +1,13 @@
+
+
+
+
+package interpreter;
+
+public class InterpreterError extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+	public InterpreterError(String msg) {
+		super(msg);
+	}
+}
